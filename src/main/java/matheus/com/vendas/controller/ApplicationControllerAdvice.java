ac2bf793591b667666166b951d28d0fdbcb8dc41
Matheus-Henrique1 <m_peu@hotmail.com>
2022-08-22 13:28:02 -0300

    Aplicando ExceptionHandler

---
+++ src/main/java/matheus/com/vendas/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,19 @@
+package matheus.com.vendas.controller;
+
+import matheus.com.vendas.ApiErrors;
+import matheus.com.vendas.exception.RegraNegocioException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(RegraNegocioException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleRegraNegocioException(RegraNegocioException exception) {
+        String mensagemErro = exception.getMessage();
+        return new ApiErrors(mensagemErro);
+    }
+}
